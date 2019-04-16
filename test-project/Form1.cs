cd1d3d2819dcee5b621e055400286e0cb1332830
afrasiyab7550 <49581979+afrasiyab7550@users.noreply.github.com>
2019-04-16 21:41:37 +0500

    Add files via upload

--- test-project/Form1.cs
+++ test-project/Form1.cs
@@ -16,5 +16,10 @@ namespace test_project
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(textBox1.Text);
+        }
     }
 }
